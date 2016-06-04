f1cee7ad12d6fe1b6d915d960bca92540c95d497
Calvin Secrest <csecrest2@gmail.com>
2016-06-04 06:46:34 -0400

    update
    This is an updated version of my personal media library

--- details.php
+++ details.php
@@ -1,12 +1,15 @@
-<?php 
-include("inc/data.php");
+<?php
+
 include("inc/functions.php");
 
 if (isset($_GET["id"])) {
-    $id = $_GET["id"];
+    $id =
+filter_input(INPUT_GET,"id",FILTER_SANITIZE_NUMBER_INT);
+    $item = single_item_array($id);
+    var_dump($item);
+}
     if (isset($catalog[$id])) {
         $item = $catalog[$id];
-    }
 }
 
 if (!isset($item)) {
@@ -22,27 +25,27 @@ include("inc/header.php"); ?>
 <div class="section page">
 
     <div class="wrapper">
-        
+
         <div class="breadcrumbs">
             <a href="catalog.php">Full Catalog</a>
             &gt; <a href="catalog.php?cat=<?php echo strtolower($item["category"]); ?>">
             <?php echo $item["category"]; ?></a>
             &gt; <?php echo $item["title"]; ?>
         </div>
-        
+
         <div class="media-picture">
-    
+
         <span>
             <img src="<?php echo $item["img"]; ?>" alt="<?php echo $item["title"]; ?>" />
         </span>
-            
+
         </div>
-        
+
         <div class="media-details">
-        
+
             <h1><?php echo $item["title"]; ?></h1>
             <table>
-            
+
                 <tr>
                     <th>Category</th>
                     <td><?php echo $item["category"]; ?></td>
@@ -71,7 +74,7 @@ include("inc/header.php"); ?>
                 <tr>
                     <th>ISBN</th>
                     <td><?php echo $item["isbn"]; ?></td>
-                </tr>    
+                </tr>
                 <?php } else if (strtolower($item["category"]) == "movies") { ?>
                 <tr>
                     <th>Director</th>
@@ -92,9 +95,9 @@ include("inc/header.php"); ?>
                 </tr>
                 <?php } ?>
             </table>
-        
+
         </div>
-    
+
     </div>
 
 </div>
--- inc/connection.php
+++ inc/connection.php
@@ -1,11 +1,12 @@
 <?php
+
 try {
-  $db = new POD("sqlite:".__DIR__."/database.db");
-  $db->setAttribute(POD::ATTR_ERRMODE,POD::ERRNODE_EXCEPTION);
+  $db = new PDO("sqlite:".__DIR__."/database.db");
+  $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
-  echo "Unable to connect"
-  echo $e->getMessage();
+  echo "Unable to connect";
+  //echo $e->getMessage();
   exit;
 }
 
-echo "Connection to the database";
+?>
